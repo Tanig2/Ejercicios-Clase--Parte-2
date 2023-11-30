15fb09ccfe6336e2291541303532cfa7dfca8a08
Tanig2 <150382668+Tanig2@users.noreply.github.com>
2023-11-30 19:26:02 +0100

    Add files via upload

---
+++ NumerosInverso.java
@@ -0,0 +1,14 @@
+package Arrays_Matrices;
+
+
+public class NumerosInverso {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	int [] vectNumeros = new int[10];
+	
+	
+	}
+
+}
